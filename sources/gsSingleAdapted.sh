33fdb09ee2a5cace2fe3d1360f3a8232f809099a
smonger <mongersteven@gmail.com>
2018-12-25 20:43:07 +1100

    update gsComparison

--- sources/gsSingleAdapted.sh
+++ sources/gsSingleAdapted.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 inputFile=$(echo $1)
-numLines=$(cat "$inputFile" | wc -l)
-numVariants=$(( $numLines / 2))
-for i in $(seq 1 $numVariants); do
-    #split into one fasta/file
-    x=$(( i * 2))
-    #fileID=$( head -${x} "$inputFile" | tail -2 | head -1 )
-    head -${x} "$inputFile" | tail -2 > temp/gsInput"$fileID".FASTA
-    output=$(bin/linux/genesplicerAdapted temp/gsInput"$fileID".FASTA human)
+while read line1 && read line2; do
+    echo -e "$line1\n$line2" > "$inputFile"SingleLine.FASTA
+    output=$(bin/linux/genesplicerAdapted "$inputFile"SingleLine.FASTA human)
     if [ ! -z "$output" ]; then
         echo "$output" >> "$inputFile"gsScoresSingle.txt
     fi
-done
+done <"$inputFile"
