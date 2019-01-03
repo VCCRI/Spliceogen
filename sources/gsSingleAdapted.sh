1f04c769479cb4c0d143ca3cceaca70a5998e349
smonger <mongersteven@gmail.com>
2019-01-03 15:14:10 +1100

    Delete gsSingleAdapted.sh

--- sources/gsSingleAdapted.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-inputFile=$(echo $1)
-while read line1 && read line2; do
-    echo -e "$line1\n$line2" > "$inputFile"SingleLine.FASTA
-    output=$(bin/linux/genesplicerAdapted "$inputFile"SingleLine.FASTA human)
-    if [ ! -z "$output" ]; then
-        echo "$output" >> "$inputFile"gsScoresSingle.txt
-    fi
-done <"$inputFile"
